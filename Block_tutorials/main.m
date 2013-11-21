e191ba991b9c9b2fe4fe77255ab276c27113890f
suiwei <evenfate@gmail.com>
2013-11-21 10:06:15 +0800

    Initial Commit

---
+++ Block_tutorials/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Block_tutorials
+//
+//  Created by ygl10 on 13-11-21.
+//  Copyright (c) 2013年 ygl10. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
